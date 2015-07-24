f72345660e0802a2f78ea4c8d9c29feb979897c5
Callym <callym.cooke@gmail.com>
2015-07-24 23:41:59 +0100

    Changed OK button to use a JSON object for settings, added useful maths defines

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,44 @@
-var gulp = require('gulp'),
-  connect = require('gulp-connect'),
-  watch = require('gulp-watch'),
-  sass = require('gulp-sass');
+var gulp 	= require('gulp'),
+	connect = require('gulp-connect'),
+	watch 	= require('gulp-watch'),
+	sass 	= require('gulp-sass'),
+	include = require('gulp-include');
 
 gulp.task('webserver', function() {
-  connect.server({
-    livereload: true,
-    root: ['build']
-  });
+	connect.server({
+		livereload: true,
+		root: ['build']
+	});
 });
 
 gulp.task('livereload', function() {
-  gulp.src(['build/styles/*.css', 'build/scripts/*.js'])
-    .pipe(watch('build/**/*'))
-    .pipe(connect.reload());
+	gulp.src(['build/styles/*.css', 'build/scripts/*.js'])
+		.pipe(watch('build/**/*'))
+		.pipe(connect.reload());
 });
 
 gulp.task('sass', function() {
-  gulp.src('src/styles/main.sass')
-    .pipe(sass())
-    .pipe(gulp.dest('build/styles'));
+	gulp.src('src/styles/main.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('build/styles'));
 });
 
 gulp.task('javascript', function() {
-  gulp.src('src/scripts/*.js')
-    .pipe(gulp.dest('build/scripts'));
+	gulp.src("src/scripts/main.js")
+		.pipe(include())
+			.on('error', console.log)
+		.pipe(gulp.dest("build/scripts"));
 });
 
 gulp.task('html', function() {
-  gulp.src('src/**/*.html')
-    .pipe(gulp.dest('build/'));
+	gulp.src('src/**/*.html')
+		.pipe(gulp.dest('build/'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('src/styles/**/*.*', ['sass']);
-  gulp.watch('src/scripts/**/*.*', ['javascript']);
-  gulp.watch('src/**/*.html', ['html']);
+	gulp.watch('src/styles/**/*.*', ['sass']);
+	gulp.watch('src/scripts/**/*.*', ['javascript']);
+	gulp.watch('src/**/*.html', ['html']);
 })
 
 gulp.task('default', ['sass', 'javascript', 'html', 'webserver', 'livereload', 'watch']);
--- src/scripts/ui/ok-button.js
+++ src/scripts/ui/ok-button.js
@@ -1,4 +1,13 @@
-$(".ok").each(function()
+/*
+<a data-cyclinglink='{ "variable": "player.appearance.hair.style", "type": "desc",
+"options": [
+"long, wavy mane",
+"short, spiky do",
+"slick, smooth bob"
+]}'></a>
+*/
+
+$("[data-setvariable]").each(function()
 {
 	var t = $(this);
 	this.innerHTML = "OK";
@@ -6,16 +15,7 @@ $(".ok").each(function()
 	t.click(function(event)
 	{
 		var t = $(this);
-		var elem = $(t.data("element"));
-
-		eval(t.data("variable") + " = '" + elem.html() + "'");
-
-		var value = $('#editablediv').attr('contenteditable');
-
-	    if (elem.attr("contenteditable") == "true")
-	    {
-	        elem.attr("contenteditable", "false");
-	    }
+		HTMLtoVariable(this);
 
 	    if (!t.data("repeat"))
 	    {
@@ -25,3 +25,38 @@ $(".ok").each(function()
 	    console.log("fired");
 	});
 });
+
+function HTMLtoVariable(t)
+{
+	var variableObject = JSON.parse(t.dataset.setvariable);
+	var v = variableObject.variable;
+	var e = $(variableObject.element)[0];
+	var vType = variableObject.type.toLowerCase();
+
+	if (vType === "desc")
+	{
+		var text = e.innerHTML;
+		var exp = v + " = '" + text + "'";
+		eval(exp);
+	}
+	else if (vType.indexOf("maths") > -1)
+	{
+		thisLink.innerHTML = options[nextIndex][0];
+		var op = " = ";
+		if 		(vType === "mathsadd") 		{ op = "ADD"; }
+		else if (vType === "mathssubtract") { op = "SUBTRACT"; }
+		else if (vType === "mathsdivide")	{ op = "DIVIDE"; }
+		else if (vType === "mathsmultiply") { op = "MULTIPLY"; }
+
+		eval(v + Maths.Operators[op] + e.innerText);
+	}
+    else if (vType.indexOf("set") > -1)
+    {
+    	eval(v + " = " + e.innerText);
+    }
+
+    if ($(e).attr("contenteditable") == "true")
+    {
+        $(e).attr("contenteditable", "false");
+    }
+}
